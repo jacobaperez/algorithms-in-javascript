d42cfbf37780207aed3481b43c398043b99c45db
Jake Perez <jacobanthony.perez@gmail.com>
2018-03-16 01:20:55 -0700

    Implemented stack with es6

---
+++ Stack.js
@@ -0,0 +1,34 @@
+
+class Stack {
+  constructor(size = Infinity) {
+    this.capacity = size;
+    this.size = 0;
+    this.storage = {};
+  }
+
+  insert(val) {
+    this.size++;
+    this.storage[this.size] = val;
+    console.log(val);
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  peek() {
+    return this.storage[this.size];
+  }
+
+  pop() {
+    if (this.size > 0) {
+      let temp = this.storage[this.size];
+      delete this.storage[this.size];
+      this.size--;
+      return temp;
+    } else {
+      console.log("Can no longer remove from empty stack");
+    }
+  }
+
+}
