3288e235657c0586dd06bd4f015f67b890a2ff39
Jake Perez <jacobanthony.perez@gmail.com>
2018-03-18 00:55:34 -0700

    complete addToHead method for linkedlist

--- linkedlist.js
+++ linkedlist.js
@@ -1,33 +1,46 @@
 
 class LinkedList {
   constructor(val) {
-    this.head = new Node(val);
-    this.tail = this.head;
-  }
-
-  addToHead(val) {
-    this.head.next = new Node(val);
-    // find better way to orient the tail
-    // this.tail = this.head.next;
-  }
-
-  forEach(cb) {
-    let node = this.head;
-    while(node) {
-      cb(node.data);
-      node = node.next;
+    if (val) {
+      this.head = new Node(val);
+      this.tail = this.head;
+    } else {
+      console.error("Please enter an initial value");
+      return;
     }
   }
 
+  // adds to head of the linked list
+  addToHead(val) {
+    let newhead = new Node(val);
+    let oldHead = this.head;
+    newhead.next = oldHead;
+    this.head = newhead;
+  }
+
+  //function used for testing.
+  check() {
+    console.log(this);
+  }
+
+
+
+  // forEach(cb) {
+  //   let node = this.head;
+  //   while(node) {
+  //     cb(node.data);
+  //     node = node.next;
+  //   }
+  // }
+
 
 }
-
 
 class Node {
   constructor(val) {
     this.data = val;
     this.next = null;
   }
-
-
 }
+
+exports.linkedlist = LinkedList;
