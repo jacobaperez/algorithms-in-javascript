6010afd36e8522b7614e41100eda6ba33b1a4497
Jake Perez <jacobanthony.perez@gmail.com>
2018-03-19 19:22:21 -0700

    start test for tree

---
+++ test/tree.spec.js
@@ -0,0 +1,19 @@
+const ds = require('../tree');
+const chai = require('chai');
+const expect = chai.expect;
+
+// let tree = ds.tree();
+
+describe("Tree", () => {
+
+  it('should create a new tree', () => {
+    expect(0).to.eql(1);
+  })
+
+
+  it('should have no children', () => {
+    expect(2).to.eql(1);
+  })
+
+
+})
